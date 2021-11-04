76dedffe857f3ae4e64662c0e8713a2186b2ab96
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2021-11-04 07:05:21 -0300

    Update verifica_servidor.sh

--- verifica_servidor.sh
+++ verifica_servidor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#adicionar script ao cron: */1 * * * * bash /scripts/verifica_servidor.sh
+#Salvar na pasta script
 codigo_http=$(curl --write-out %{http_code} --silent --output /dev/null www.site.com.br)
 
 if [ $codigo_http -ne 200 ]; then
